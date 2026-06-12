7d68e2d3da0135d2ac0d38211a4a5c4142c3fb13
Ritesh <riteshroyal834047@gmail.com>
2026-06-12 14:21:41 +0530

    chore: initialize MongoDB practice repository

---
+++ Crud operation/crud.mongodb.js
@@ -0,0 +1,15 @@
+
+
+// const res1=show("dbs")
+// const res2=use("ecommerce")
+
+// console.log(res1)
+// console.log(res2)
+
+show("dbs")
+use("ecommerce")
+show("collections")
+
+db.users.insertOne({
+    name:"alpha"
+})
