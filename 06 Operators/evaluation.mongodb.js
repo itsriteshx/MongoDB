cf037f366138b999fb46b7d2bed7a6d7d7930e90
Ritesh <riteshroyal834047@gmail.com>
2026-06-17 22:49:33 +0530

    comments out

--- 06 Operators/evaluation.mongodb.js
+++ 06 Operators/evaluation.mongodb.js
@@ -1,27 +1,27 @@
 use('test')
 
-db.employees.insertMany([
-  {
-    name: "Rahul",
-    salary: 50000,
-    bonus: 10000
-  },
-  {
-    name: "Priya",
-    salary: 60000,
-    bonus: 5000
-  },
-  {
-    name: "Amit",
-    salary: 45000,
-    bonus: 15000
-  },
-  {
-    name: "Neha",
-    salary: 70000,
-    bonus: 2000
-  }
-])
+// db.employees.insertMany([
+//   {
+//     name: "Rahul",
+//     salary: 50000,
+//     bonus: 10000
+//   },
+//   {
+//     name: "Priya",
+//     salary: 60000,
+//     bonus: 5000
+//   },
+//   {
+//     name: "Amit",
+//     salary: 45000,
+//     bonus: 15000
+//   },
+//   {
+//     name: "Neha",
+//     salary: 70000,
+//     bonus: 2000
+//   }
+// ])
 
 db.employees.updateOne(
     { name: 'Rahul' },
