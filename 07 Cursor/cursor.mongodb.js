19960523fbc6be822b6ab148b0b44852c5f7b982
Ritesh <riteshroyal834047@gmail.com>
2026-06-24 17:21:08 +0530

    updates some

--- 07 Cursor/cursor.mongodb.js
+++ 07 Cursor/cursor.mongodb.js
@@ -1,9 +1,9 @@
 use("test")
-let arr=[]
-for (let i=1; i<=100; i++){
-    arr.push({value: i})
-}
-db.data.insertMany(arr)
+// let arr=[]
+// for (let i=1; i<=100; i++){
+//     arr.push({value: i})
+// }
+// db.data.insertMany(arr)
 // const cursor=db.data.find()
 // console.log(cursor)
 
