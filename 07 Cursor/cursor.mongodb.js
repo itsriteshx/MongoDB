73ef8da512b73e7b2ad38097a66dce23dcae77a2
Ritesh <riteshroyal834047@gmail.com>
2026-06-24 18:14:15 +0530

    comment line

--- 07 Cursor/cursor.mongodb.js
+++ 07 Cursor/cursor.mongodb.js
@@ -12,15 +12,15 @@ use("test")
 // console.log(cursor.next())
 // console.log(cursor.hasNext())
 
-while(cursor.hasNext()){
-    console.log(cursor.next())
-}
+// while(cursor.hasNext()){
+//     console.log(cursor.next())
+// }
 
 // db.data.find().sort({value:1}) //ascending sort
 // db.data.find().sort({value:-1}) //descending sort
 
-db.data.find().limit(40)
-db.data.find().skip(90)
+// db.data.find().limit(40)
+// db.data.find().skip(90)
 
 // const data=db.data.find().sort({value: -1}).skip(5).limit(8)
 // console.log(data)
