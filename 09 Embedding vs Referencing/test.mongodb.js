74dff6ffef493c8107e86014d6e59341c871fb18
Ritesh <riteshroyal834047@gmail.com>
2026-07-04 12:30:52 +0530

    all concept of refercering

--- 09 Embedding vs Referencing/test.mongodb.js
+++ 09 Embedding vs Referencing/test.mongodb.js
@@ -12,3 +12,56 @@ db.students.insertOne({
         },
     ]
 })
+
+ db.students.find(
+    { 'courses.name': 'Graphic Design' }
+)
+
+db.students.insertOne({
+    _id: 3,
+    name:'mohit',
+    address:{
+        city:'bhagalpur',
+        state:'bihar',
+        pincode:812004,
+    }
+})
+
+
+db.students.updateOne(
+    {'address.city':'bhagalpur'},
+    {$set: {'address.city':'patna'}}
+)
+
+db.students.find()
+
+db.students.find({},{'address.city':1, _id: 0})
+
+
+db.courses.insertOne({
+    _id: 2,
+    name: 'Graphic Design',
+    price: 150000,
+    duration: 1
+})
+
+db.courses.find()
+
+db.students.insertOne({
+    _id:5,
+    name:'maha faltu',
+    courses:[1, 2]
+})
+
+db.students.find();
+
+db.students.aggregate([
+    {
+        $lookup:{
+            from: 'courses',
+            localField:'courses',
+            foreignField: '_id',
+            as:'coursesDetails'
+        }
+    }
+])
