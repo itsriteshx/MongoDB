58fd9965a54d034e461fbf2605f42ea10ea13d9f
Ritesh <riteshroyal834047@gmail.com>
2026-07-11 15:18:53 +0530

    solve quetsion 1

--- 12 Aggeration Question/question.mongodb.js
+++ 12 Aggeration Question/question.mongodb.js
@@ -1,20 +1,236 @@
+use("test")
+// db.orders.insertMany([
+//     {
+//         customer: "Manas",
+//         city: "Patna",
+//         products: [
+//             { name: "Laptop", category: "Electronics", price: 50000, quantity: 1 },
+//             { name: "Mouse", category: "Electronics", price: 1000, quantity: 2 },
+//             { name: "Keyboard", category: "Electronics", price: 2500, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
 
-// ***********QUESTIONS***************
-// Q.1 Find total sales of each category
-// Q.2 Find top spending customer
-// Q.3 Show all products bought by each customer
-// Q.4 Find customers who spent more than 50,000
-// Q.5 Find most sold product
-// Q.6 Create pagination using aggregation
-// Q.7 Find average order value per city
+//     {
+//         customer: "Rahul",
+//         city: "Delhi",
+//         products: [
+//             { name: "Shoes", category: "Fashion", price: 3000, quantity: 2 }
+//         ],
+//         paymentMethod: "Card",
+//         status: "Delivered"
+//     },
 
-/* Q.8 Create analytics dashboard data.
-        For each category:
-            total products sold
-            total revenue
-            average price
-*/
+//     {
+//         customer: "Priya",
+//         city: "Mumbai",
+//         products: [
+//             { name: "Phone", category: "Electronics", price: 35000, quantity: 1 },
+//             { name: "Cover", category: "Accessories", price: 500, quantity: 2 },
+//             { name: "Charger", category: "Electronics", price: 1200, quantity: 1 }
+//         ],
+//         paymentMethod: "COD",
+//         status: "Pending"
+//     },
 
-// Q.9 Join users and orders using $lookup
-// Q.10 Find users having no orders
+//     {
+//         customer: "Manas",
+//         city: "Patna",
+//         products: [
+//             { name: "Monitor", category: "Electronics", price: 15000, quantity: 1 }
+//         ],
+//         paymentMethod: "COD",
+//         status: "Pending"
+//     },
 
+//     {
+//         customer: "Sneha",
+//         city: "Patna",
+//         products: [
+//             { name: "Bag", category: "Fashion", price: 1800, quantity: 1 },
+//             { name: "Perfume", category: "Beauty", price: 2200, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Rahul",
+//         city: "Delhi",
+//         products: [
+//             { name: "T-Shirt", category: "Fashion", price: 1200, quantity: 3 },
+//             { name: "Jeans", category: "Fashion", price: 2500, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Aman",
+//         city: "Kolkata",
+//         products: [
+//             { name: "Watch", category: "Accessories", price: 5000, quantity: 1 }
+//         ],
+//         paymentMethod: "Card",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Neha",
+//         city: "Mumbai",
+//         products: [
+//             { name: "Face Wash", category: "Beauty", price: 500, quantity: 3 },
+//             { name: "Nail Paint", category: "Beauty", price: 300, quantity: 5 }
+//         ],
+//         paymentMethod: "Card",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Priya",
+//         city: "Mumbai",
+//         products: [
+//             { name: "Lipstick", category: "Beauty", price: 800, quantity: 4 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Vikash",
+//         city: "Delhi",
+//         products: [
+//             { name: "Tablet", category: "Electronics", price: 20000, quantity: 1 },
+//             { name: "Keyboard", category: "Electronics", price: 3000, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Arjun",
+//         city: "Patna",
+//         products: [
+//             { name: "Headphones", category: "Electronics", price: 2500, quantity: 2 }
+//         ],
+//         paymentMethod: "Card",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Manas",
+//         city: "Patna",
+//         products: [
+//             { name: "Speaker", category: "Electronics", price: 6000, quantity: 1 },
+//             { name: "Gaming Mouse", category: "Electronics", price: 3500, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Rohit",
+//         city: "Delhi",
+//         products: [
+//             { name: "TV", category: "Electronics", price: 45000, quantity: 1 },
+//             { name: "Sound Bar", category: "Electronics", price: 8000, quantity: 1 }
+//         ],
+//         paymentMethod: "COD",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Sneha",
+//         city: "Patna",
+//         products: [
+//             { name: "Heels", category: "Fashion", price: 2800, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Pending"
+//     },
+
+//     {
+//         customer: "Aman",
+//         city: "Kolkata",
+//         products: [
+//             { name: "Perfume", category: "Beauty", price: 2200, quantity: 2 },
+//             { name: "Face Cream", category: "Beauty", price: 1200, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Cancelled"
+//     },
+
+//     {
+//         customer: "Pooja",
+//         city: "Bangalore",
+//         products: [
+//             { name: "Dress", category: "Fashion", price: 4000, quantity: 1 },
+//             { name: "Handbag", category: "Fashion", price: 3500, quantity: 1 }
+//         ],
+//         paymentMethod: "Card",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Neha",
+//         city: "Mumbai",
+//         products: [
+//             { name: "Hair Dryer", category: "Electronics", price: 2500, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Pending"
+//     },
+
+//     {
+//         customer: "Arjun",
+//         city: "Patna",
+//         products: [
+//             { name: "Speaker", category: "Electronics", price: 6000, quantity: 1 },
+//             { name: "Mic", category: "Electronics", price: 2500, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Vikash",
+//         city: "Delhi",
+//         products: [
+//             { name: "Monitor", category: "Electronics", price: 15000, quantity: 1 }
+//         ],
+//         paymentMethod: "COD",
+//         status: "Delivered"
+//     },
+
+//     {
+//         customer: "Rohit",
+//         city: "Delhi",
+//         products: [
+//             { name: "Gaming Mouse", category: "Electronics", price: 3500, quantity: 2 },
+//             { name: "Mouse Pad", category: "Accessories", price: 700, quantity: 1 }
+//         ],
+//         paymentMethod: "UPI",
+//         status: "Pending"
+//     }
+// ])
+
+
+// ques 1
+db.orders.aggregate([
+    {
+        $unwind: "$products"
+    },
+    {
+        $group: {
+          _id: "$products.category",
+          totalSale:{
+            $sum:{
+                $multiply:["$products.price","$products.quantity"]
+            }
+          }
+        }
+    }
+
+
+])
